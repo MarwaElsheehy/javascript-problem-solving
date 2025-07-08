32b146b88411dfbe23ed1b7582479eedd654891f
Marwa <118805027+MarwaElsheehy@users.noreply.github.com>
2025-07-08 23:11:48 +0300

    Create Color Ghost.js

---
+++ Color Ghost.js
@@ -0,0 +1,8 @@
+class Ghost {
+  constructor() {
+    const colors = ["white", "yellow", "purple", "red"];
+    this.color = colors[Math.floor(Math.random() * colors.length)];
+  }
+}
+const ghost = new Ghost();
+console.log(ghost.color);
