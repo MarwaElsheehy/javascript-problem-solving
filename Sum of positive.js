cfa37a023a92ed7fcef7767c772b49d6b4739b4e
Marwa <118805027+MarwaElsheehy@users.noreply.github.com>
2025-07-14 20:44:15 +0300

    Update Sum of positive.js

--- Sum of positive.js
+++ Sum of positive.js
@@ -1,10 +1,6 @@
 function positiveSum(arr) {
   const positiveNumbers = arr.filter(num => num >= 0);
-  if (positiveNumbers.length === 0) {
-    return 0;
-  } else {
-    return positiveNumbers.reduce((acc, curr) => acc + curr);
-  }
+  return positiveNumbers.reduce((acc, curr) => acc + curr, 0);
 }
 console.log(positiveSum([1, -4, 7, 12]));
 console.log(positiveSum([-1,-2,-3,-4,-5]));
