aeecd78235947aa4134218a38df15103576c27ca
Marwa <118805027+MarwaElsheehy@users.noreply.github.com>
2025-07-16 00:31:05 +0300

    Update They say that only the name is long enough to attract attention. They also said that only a simple Kata will have someone to solve it. This is a sadly story #1: Are they opposite?.js

--- They say that only the name is long enough to attract attention. They also said that only a simple Kata will have someone to solve it. This is a sadly story #1: Are they opposite?.js
+++ They say that only the name is long enough to attract attention. They also said that only a simple Kata will have someone to solve it. This is a sadly story #1: Are they opposite?.js
@@ -9,3 +9,9 @@ function isOpposite(s1, s2) {
 }
 console.log(isOpposite("ab", "AB"));
 console.log(isOpposite("Ab", "cD"));
+
+
+function isOpposite(s1, s2) {
+  if (!s1 || s1.length !== s2.length) return false;
+  return [...s1].every((c, i) => c !== s2[i] && c.toLowerCase() === s2[i].toLowerCase());
+}
