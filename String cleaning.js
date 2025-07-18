ba9769fb0de1072f837c9992369003a6718f723a
Marwa <118805027+MarwaElsheehy@users.noreply.github.com>
2025-07-18 22:43:43 +0300

    Create String cleaning.js

---
+++ String cleaning.js
@@ -0,0 +1,5 @@
+function stringClean(s){
+  return s.replace(/[0-9]/g, '');
+}
+console.log(stringClean("A1 A1! AAA   3J4K5L@!!!"));
+console.log(stringClean("Wh7y can't we3 bu1y the goo0d software3? #cheapskates3"));
