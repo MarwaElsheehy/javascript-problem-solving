255cb61bcf2b6195a4e86bedf7ad645655bdf4de
Marwa <118805027+MarwaElsheehy@users.noreply.github.com>
2025-08-03 23:27:26 +0300

    Update Reverser.js

--- Reverser.js
+++ Reverser.js
@@ -1,3 +1,12 @@
+function reverse(n) {
+  let reversed = 0;
+  while (n !== 0) {
+    reversed = reversed * 10 + n % 10;
+    n = Math.floor(n / 10);
+  }
+  return reversed;
+}
+
 function reverse(n, reversed = 0) {
     if (n === 0) {
         return reversed;
